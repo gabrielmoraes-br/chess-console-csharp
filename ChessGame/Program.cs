50fa3044f8be6886054eae5cbff5c60e1674ccc5
Gabriel Moraes <gabrielmoraes.dev@gmail.com>
2022-08-11 22:21:59 -0300

    Implementation of the chess board positions to the console

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -14,9 +14,13 @@ namespace ChessGame
             {
                 Board board = new Board(8, 8);
 
-                board.PutPiece(new Rook(Color.White, board), new Position(0, 0));
-                board.PutPiece(new Rook(Color.White, board), new Position(1, 3));
+                board.PutPiece(new Rook(Color.White, board), new Position(5, 1));
+                board.PutPiece(new Knight(Color.Black, board), new Position(1, 3));
                 board.PutPiece(new King(Color.Black, board), new Position(0, 2));
+                board.PutPiece(new Bishop(Color.Black, board), new Position(3, 7));
+                board.PutPiece(new Pawn(Color.White, board), new Position(4, 4));
+                board.PutPiece(new Queen(Color.White, board), new Position(7, 2));
+                board.PutPiece(new King(Color.White, board), new Position(7, 3));
 
                 Screen.PrintBoard(board);
             }
--- ChessGame/Screen.cs
+++ ChessGame/Screen.cs
@@ -1,4 +1,5 @@
 using ChessGame.Entities.Chess;
+using ChessGame.Entities.Enums;
 
 namespace ChessGame;
 
@@ -8,6 +9,7 @@ public class Screen
     {
         for (int i = 0; i < board.Lines; i++)
         {
+            Console.Write($"{8 - i} ");
             for (int j = 0; j < board.Columns; j++)
             {
                 if (board.Piece(i, j) == null)
@@ -16,10 +18,27 @@ public class Screen
                 }
                 else
                 {
-                    Console.Write($"{board.Piece(i,j)} ");
+                    PrintPiece(board.Piece(i, j));
+                    Console.Write(" ");
                 }
             }
             Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void PrintPiece(Piece piece)
+    {
+        if (piece.Color == Color.White)
+        {
+            Console.Write(piece);
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
         }
     }
 }
